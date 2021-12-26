fbed0bc2cc5816800967ef3f16671a41cf42fdce
saky-office <77478784+saky-office@users.noreply.github.com>
2021-12-26 18:03:52 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,24 @@ var iUp = (function () {
 	}
 })();
 
+function getBingImages(imgUrls) {
+	/**
+	 * 获取Bing壁纸
+	 * 先使用 GitHub Action 每天获取 Bing 壁纸 URL 并更新 images.json 文件
+	 * 然后读取 images.json 文件中的数据
+	 */
+	var indexName = "bing-image-index";
+	var index = sessionStorage.getItem(indexName);
+	var $panel = $('#panel');
+	if (isNaN(index) || index == 7) index = 0;
+	else index++;
+	var imgUrl = imgUrls[index];
+	var url = "https://www.bing.com" + imgUrl;
+	$panel.css("background", "url('" + url + "') center center no-repeat #666");
+	$panel.css("background-size", "cover");
+	sessionStorage.setItem(indexName, index);
+}
+
 $(document).ready(function () {
 
 	// 获取一言数据
